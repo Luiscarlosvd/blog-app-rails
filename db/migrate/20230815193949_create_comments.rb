a5721f969e91cc8206844165e3a2560f2e6838eb
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-15 16:11:02 -0400

    Add migration files to create Likes table

--- db/migrate/20230815193949_create_comments.rb
+++ db/migrate/20230815193949_create_comments.rb
@@ -2,10 +2,10 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :posts, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :comments, :posts
+    add_index :comments, :post
   end
 end
