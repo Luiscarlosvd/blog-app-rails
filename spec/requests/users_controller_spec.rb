d3b6f1ea39781d2e2a0b1c5d5e3a1d57bdbc09b3
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-27 12:05:37 -0400

    Update tests in requests folder

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,23 +2,26 @@ require './spec/rails_helper'
 
 RSpec.describe UsersController, type: :request do
   describe 'GET #show' do
+    let(:user) { User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'teacher from Mexico', posts_counter: 0) }
+    let(:post) { Post.create(title: 'Post Title', text: 'Post Text', author: user) }
     it 'renders a successful response' do
-      get '/users/show'
+      get "/users/#{user.id}"
       expect(response.status).to eq(200)
     end
 
     it 'renders the show template' do
-      get '/users/show'
+      get "/users/#{user.id}"
       expect(response).to render_template('show')
     end
 
     it 'renders the correct placeholder in the response body' do
-      get '/users/show'
-      expect(response.body).to include('Here is the description and posts for a given user')
+      get "/users/#{user.id}"
+      expect(response.body).to include('John Doe')
     end
   end
 
   describe 'GET #index' do
+    let!(:user) { User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'teacher from Mexico', posts_counter: 0) }
     it 'renders a successful response' do
       get '/users'
       expect(response.status).to eq(200)
@@ -31,7 +34,7 @@ RSpec.describe UsersController, type: :request do
 
     it 'renders the correct placeholder in the response body' do
       get '/users'
-      expect(response.body).to include('Here is a list of all users')
+      expect(response.body).to include('John Doe')
     end
   end
 end
--- spec/views/index_posts_spec.rb
+++ spec/views/index_posts_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.describe 'users/index', type: :system do
   describe 'index page' do
     before do
-        User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'teacher from Mexico', posts_counter: 5)
-        User.create(name: 'Jane Smith', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'teacher from Colombia', posts_counter: 3)
-    
-        visit users_path
-      end
-    
-      it 'displays the username of all other users' do
-        expect(page).to have_selector('h2', text: 'John Doe')
-        expect(page).to have_selector('h2', text: 'Jane Smith')
-      end
+      User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'teacher from Mexico',
+                  posts_counter: 5)
+      User.create(name: 'Jane Smith', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'teacher from Colombia',
+                  posts_counter: 3)
+
+      visit users_path
+    end
+
+    it 'displays the username of all other users' do
+      expect(page).to have_selector('h2', text: 'John Doe')
+      expect(page).to have_selector('h2', text: 'Jane Smith')
+    end
   end
 end
