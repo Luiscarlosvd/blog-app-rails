7cbde69daea60418f398aeafca67fb3c74e0c03d
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-29 14:38:46 -0400

    Fix linters

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,2 @@
 class Users::SessionsController < Devise::SessionsController
-  def new
-    super
-    flash[:notice] = "You can confirm your account email through the link below: #{view_context.link_to('Confirm my account', user_confirmation_url(confirmation_token: current_user.confirmation_token))}" if current_user && !current_user.confirmed?
-  end
-  def destroy
-    super
-  end
 end
